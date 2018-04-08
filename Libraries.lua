9f475576a89949990aecfcef7d6b41576c39b10f
Validark <NarrevRBX@gmail.com>
2018-04-07 18:25:10 -0600

    Create Libraries.lua

---
+++ Libraries.lua
@@ -0,0 +1,15 @@
+return {
+	RoStrap = {
+		URL = "RoStrap" -- Import everything under the organization `RoStrap`
+	};
+
+	Try = {
+		URL = "Validark/RBX-Try-Library/blob/patch-4";
+		Description = "A library for controlling the flow of error-prone, interdependent functions";
+	};
+
+	Rbx_CustomFont = {
+		URL = "Gskartwii/Rbx_CustomFont";
+		Description = "Custom Fonts";
+	};
+}
